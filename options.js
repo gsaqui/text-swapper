e795f14d09f85626867e360ebb26766e04312d78
gsaqui <glenn.saqui@gmail.com>
2016-10-19 16:23:22 -0700

    start wiring in the options panel

--- options.js
+++ options.js
@@ -1,18 +1,24 @@
 function saveOptions(e) {
+    console.log('calling save');
   chrome.storage.local.set({
     colour: document.querySelector("#colour").value
   });
 }
 
-function restoreOptions() {
+function loadSwappingText() {
   chrome.storage.local.get('selectedText', (res) => {
     console.log(res);
-    document.querySelector("#colour").value = res.selectedText || 'Firefox red';
+
+console.log($('#selected_colour_num'));
+    $('#selected_colour_num').val(res.selectedText);
+    // document.querySelector("#colour").value = res.selectedText || 'Firefox red';
   });
 }
 
-document.addEventListener('DOMContentLoaded', restoreOptions);
-document.querySelector("form").addEventListener("submit", saveOptions);
+$(document).ready(loadSwappingText);
+$('#saveBtn').click(saveOptions)
+//document.addEventListener('DOMContentLoaded', restoreOptions);
+//document.querySelector("form").addEventListener("submit", saveOptions);
 
 
 // var myPort = chrome.runtime.connect({name:"port-from-options"});
